50948bbca9ca73735ffd79fbf82d683be7832438
maphestus <lockecmu@gmail.com>
2019-08-14 22:02:50 -0400

    Added morale / routing

--- army.py
+++ army.py
@@ -39,11 +39,24 @@ class Combat:
                 armydefense.hp = armydefense.hp - armybdmg
             print(f'{armyattack.name} did {armybdmg} to {armydefense.name}, hp now {armydefense.hp}')
             print(f'{armydefense.name} did {armyadmg} to {armyattack.name}, hp now {armyattack.hp}')
+
+            # Morale Check
+            if armyattack.hp <= armyattack.acr:
+                routa = sum(dice.roll('1d20')) + armyattack.morale
+                if routa < 15:
+                    armyattack.hp = 0
+                    print(f'{armyattack.name} routed!')
+            if armydefense.hp <= armydefense.acr:
+                routb = sum(dice.roll('1d20')) + armydefense.morale
+                if routb < 15:
+                    armydefense.hp = 0
+                    print(f'{armydefense.name} routed!')
+
             rounds = rounds + 1
 
 
-OrcArmy = Army("Orcs", 100, "orcs", 11, 13, 2, 2, 0)
-HumArmy = Army("Human", 100, "human", 16, 13, 3, 3, 0)
+OrcArmy = Army("Orcs", 100, "orc", 11, 13, 2, 2, 0)
+HumArmy = Army("Humans", 100, "human", 16, 13, 3, 3, 0)
 
 # print(OrcArmy.__dict__)
 # print(HumArmy.__dict__)
